87d960326b3a47c73c777bf6274f2e2ff1d2b913
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-03-27 21:59:16 -0300

    Dei Função ao Botão no Login e tirei a Barra superior

--- app/src/main/java/br/com/etecia/appempresaabc/LoginActivity.java
+++ app/src/main/java/br/com/etecia/appempresaabc/LoginActivity.java
@@ -1,14 +1,17 @@
 package br.com.etecia.appempresaabc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText txtUsuario, txtSenha;
+    Button sairSistema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,18 @@ public class LoginActivity extends AppCompatActivity {
 
         txtUsuario = (EditText) findViewById(R.id.edtUsuario);
         txtSenha = (EditText) findViewById(R.id.edtSenha);
+
+        sairSistema = (Button) findViewById(R.id.btnSair);
+
+        sairSistema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
-    public void sairApp(View view) {
-        finish();
-    }
 
     public void entrarApp(View view) {
         String usuario = txtUsuario.getText().toString();
@@ -29,6 +39,8 @@ public class LoginActivity extends AppCompatActivity {
 
         if (usuario.equals("etecia") && senha.equals("etecia")) {
             Toast.makeText(getApplicationContext(),"Bem Vindo ao Sistema", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MenuPrincipalActivity.class);
+            startActivity(intent);
         } else {
             Toast.makeText(getApplicationContext(), "Usuário ou Senha invalidos", Toast.LENGTH_SHORT).show();
         }
